a4917cab7feab82c4b18c802bdbf7d9354dd6286
최지민 <choijming21@naver.com>
2025-04-15 14:22:38 +0900

    feat: 페이지 기본 구조 설정

---
+++ src/app/dashboard/coin/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CoinIdPage = () => {
+  return <div>CoinIdPage</div>;
+};
+
+export default CoinIdPage;
---
+++ src/app/dashboard/stock/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StockIdPage = () => {
+  return <div>StockIdPage</div>;
+};
+
+export default StockIdPage;
